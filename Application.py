87848830af3dafa751f1bf95334c96735065c09b
chetan <chetan@gmail.com>
2025-09-04 12:54:20 +0530

    Initial commit: basic Flask app for ACEest Fitness and GYM

---
+++ Application.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# In-memory list to store registered members
+members = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+
+        if name and email:
+            members.append({'name': name, 'email': email})
+            return redirect(url_for('list_members'))
+        else:
+            return render_template('register.html', error="Please Enter name and email.")
+
+    return render_template('register.html')
+
+@app.route('/members')
+def list_members():
+    return render_template('members.html', members=members)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+This is a initial Test application
+Created with support form web for simple html files 
+
+index.html contains title body and 2 url pages
+members.html and register.html
+
+members.html holds the objects for each of them 
+register.html gives pages to register
